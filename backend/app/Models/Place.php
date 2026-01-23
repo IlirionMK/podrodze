09acc10f5fa22c89868f6d1a978b5c832b12f544
WroblewskiSebastian <sebastian.wroblewski@studenci.collegiumwitelona.pl>
2026-01-23 01:51:05 +0100

    #T16 Added Factories

--- backend/app/Models/Place.php
+++ backend/app/Models/Place.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Place extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'name',
         'google_place_id',
--- backend/app/Models/UserPreference.php
+++ backend/app/Models/UserPreference.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserPreference extends Model
 {
+    use HasFactory;
     protected $fillable = ['user_id', 'category_id', 'score'];
 
     public function user()
